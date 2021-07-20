2fe9a055924d7b85fb417041ad2ed6f599710f70
AdiPol1359 <adrian.polak20@gmail.com>
2021-07-20 23:01:52 +0200

    Update Main.ts

--- src/Main.ts
+++ src/Main.ts
@@ -2,6 +2,8 @@ import RakNetListener from './raknet/RakNetListener';
 
 const listener = new RakNetListener().listen(19132);
 
+listener.on('start', () => console.log('[Coffee RakNet] Server started!'));
+
 listener.on('unconnectedPing', rinfo => {
-    listener.pong(rinfo, 'Coffee Server', '390', '1.17.2', '0', '15', '19132');
+    listener.pong(rinfo, 'Coffee Server', '390', '1.17.2', '0', '10', '19132');
 });
