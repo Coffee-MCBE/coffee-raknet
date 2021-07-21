58c8970366e17189fd0f836110bcabafe37f5f37
AdiPol1359 <adrian.polak20@gmail.com>
2021-07-21 17:12:25 +0200

    Add new protocols

--- src/raknet/util/ProtocolInfo.ts
+++ src/raknet/util/ProtocolInfo.ts
@@ -1,6 +1,10 @@
+import OpenConnectionReply1 from "../protocol/OpenConnectionReply1";
+
 export default {
     UnconnectedPing: 0x01,
     UnconnectedPong: 0x1c,
     OpenConnectionRequest1: 0x05,
-    OpenConnectionRequest2: 0x07
+    OpenConnectionReply1: 0x06,
+    OpenConnectionRequest2: 0x07,
+    OpenConnectionReply2: 0x08
 }
