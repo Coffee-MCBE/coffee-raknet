a6cd2be2cbfda1bb9809f69393dfeb5ce76a89b5
AdiPol1359 <adrian.polak20@gmail.com>
2021-07-21 17:09:32 +0200

    Create InetAddress methods

--- src/raknet/protocol/Packet.ts
+++ src/raknet/protocol/Packet.ts
@@ -1,4 +1,5 @@
 import BinaryStream from '../util/BinaryStream';
+import InetAddress from '../util/InetAddress';
 
 class Packet extends BinaryStream {
 
@@ -20,6 +21,27 @@ class Packet extends BinaryStream {
 
     public encodePayload() : void {
         this.writeByte(this.id);
+    }
+
+    //TODO: ADD SUPPORT FOR ipv6
+    public readAddress() : InetAddress|null {
+        const version = this.readByte();
+        
+        if(version === 4) {
+            const address = `${(~this.readByte()) & 0xff}.${(~this.readByte()) & 0xff}.${(~this.readByte()) & 0xff}.${(~this.readByte()) & 0xff}`;
+            const port = this.readUnsignedShort();
+
+            return new InetAddress(address, port, version);
+        }
+
+        return null;
+    }
+
+    //TODO: ADD SUPPORT FOR ipv6
+    public writeAddress(address: InetAddress) {
+        this.writeByte(address.getVersion());
+        address.getAddress().split('.', 4).forEach(n => this.writeByte((~parseInt(n)) & 0xff));
+        this.writeUnsignedShort(address.getPort());
     }
 }
 
