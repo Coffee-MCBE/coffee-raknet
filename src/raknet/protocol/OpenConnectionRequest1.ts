d00bba5a2bc4110a30a1d0a03d463efa79c79c7d
AdiPol1359 <adrian.polak20@gmail.com>
2021-07-21 17:10:26 +0200

    Fix mtuSize

--- src/raknet/protocol/OpenConnectionRequest1.ts
+++ src/raknet/protocol/OpenConnectionRequest1.ts
@@ -14,14 +14,14 @@ class OpenConnectionRequest1 extends OfflinePacket {
     decodePayload() {
         super.decodePayload();
 
-        this.mtuSize = (Buffer.byteLength(this.buffer) + 1) + 28;;
+        this.mtuSize = (Buffer.byteLength(this.buffer) + 1) + 28;
         this.readMagic();
         this.protocolVersion = this.readByte();
     }
 
     encodePayload() {
         super.encodePayload();
-        
+
         this.writeMagic();
         this.writeByte(this.protocolVersion);
         this.write(Buffer.alloc(this.mtuSize - this.buffer.length).fill(0x00));
