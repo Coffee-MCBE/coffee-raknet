5073a7c904a27ae75e4897768a3c7fec7540b38a
AdiPol1359 <adrian.polak20@gmail.com>
2021-07-21 19:33:53 +0200

    Complete packet

--- src/raknet/protocol/OpenConnectionRequest2.ts
+++ src/raknet/protocol/OpenConnectionRequest2.ts
@@ -1,8 +1,34 @@
 import OfflinePacket from './OfflinePacket';
+import ProtocolInfo from '../util/ProtocolInfo';
+import InetAddress from '../util/InetAddress';
 
 class OpenConnectionRequest2 extends OfflinePacket {
 
-    
+    declare public address: InetAddress;
+    declare public mtuSize: number;
+    declare public clientGuid: bigint;
+
+    constructor() {
+        super(ProtocolInfo.OpenConnectionRequest2);
+    }
+
+    decodePayload() {
+        super.decodePayload();
+
+        this.readMagic();
+        this.address = this.readAddress();
+        this.mtuSize = this.readShort();
+        this.clientGuid = this.readLong();
+    }
+
+    encodePayload() {
+        super.encodePayload();
+
+        this.writeMagic();
+        this.writeAddress(this.address);
+        this.writeShort(this.mtuSize);
+        this.writeLong(this.clientGuid);
+    }
 }
 
 export default OpenConnectionRequest2;
